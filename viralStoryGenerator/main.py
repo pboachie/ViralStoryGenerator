ca2d8b2e315ae3bacf99e7764b117882b6ac8d82
pboachie <princeboachie@gmail.com>
2025-03-29 13:07:31 -0700

    refactor: replace basic logging setup with centralized logger for improved consistency

--- viralStoryGenerator/main.py
+++ viralStoryGenerator/main.py
@@ -1,13 +1,10 @@
 # viralStoryGenerator/main.py
-import logging
+from viralStoryGenerator.src.logger import logger as _logger
 from viralStoryGenerator.src.cli import cli_main
 
 
 if __name__ == "__main__":
 
     # Configure basic logging
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    _logger = logging.getLogger(__name__)
-
-    _logger.info("Starting the Viral Story Generator CLI...")
+    _logger.debug("Starting the Viral Story Generator CLI...")
     cli_main()
